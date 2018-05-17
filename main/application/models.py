29094d964d3f68bc56027a1f9a64a5ca07e8c4e0
Emanuele Lillo <emalillo270304@gmail.com>
2018-05-17 21:54:20 +0200

    New Beta Structure

--- main/application/models.py
+++ main/application/models.py
@@ -1,9 +1,13 @@
-from flask_restplus import fields
+from flask_restplus import fields, Namespace
 from .api import api
+
+auth_ns = Namespace('Authorizing', description="Login related operations.")
 
 models = {
     "account": api.model("Account Credentials", {
-        "id": fields.String("The Account's ID."),
-        "password": fields.String("The Account's password.")
+        "id": fields.String(required=True, "123456789"),
+        "password": fields.String(required=True, "login_me!")
     })
 }
+
+api.add_namespace(auth_ns)
---
+++ main/beta.py
@@ -0,0 +1,7 @@
+from beta_application import app
+
+if __name__ == '__main__':
+    app.run(
+        port=5000,
+        debug=True
+    )
