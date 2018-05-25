7181c97c1e7e28510a8290a3e333ad03c2b78e70
Emanuele Lillo <emalillo270304@gmail.com>
2018-05-25 12:00:12 +0200

    Maybe this is the Bug Fix

--- src/application/utils/database.py
+++ src/application/utils/database.py
@@ -5,47 +5,61 @@ from hashlib import sha512
 import string, random
 from datetime import datetime
 
-connection = sql.connect(DATABASE_PATH, check_same_thread=False)
-cursor = connection.cursor()
+def connectDatabase():
+    connection = sql.connect(DATABASE_PATH, check_same_thread=False)
+    cursor = connection.cursor()
+    return connection, cursor
 
 def hashTok(tok):
     return sha512(tok.encode()).hexdigest()
 
 def getHashedPassword(id):
+    connection, cursor = connectDatabase()
     cursor.execute("SELECT * FROM credentials WHERE id = ?", (id,))
     return cursor.fetchone()[1]
+    connection.close()
 
 def getBalance(id):
+    connection, cursor = connectDatabase()
     cursor.execute("SELECT * FROM balances WHERE 'id' = ?", (id,))
-    result = cursor.fetchall()
+    result = cursor.fetchone()
     print("Balance:", result) # DEBUG: SHOW ALL ROWS
-    return result[0][1]
+    connection.close()
+    return result[1]
 
 def addBalance(id, quantity):
+    connection, cursor = connectDatabase()
     balance = getBalance(id)
     new_balance = int(balance) + quantity
     cursor.execute("UPDATE balances SET quantity = ? WHERE id = ?", (new_balance, id))
-    connecton.commit()
+    connection.commit()
+    connection.close()
 
 def createUser(id, password):
+    connection, cursor = connectDatabase()
     hashed_password = hashTok(password)
     cursor.execute("INSERT INTO credentials ('id', 'passoword') VALUES (?, ?)", (id, hashed_password))
     cursor.execute("INSERT INTO balances ('id', 'quantity') VALUES (?, 0)", (id,))
     connection.commit()
+    connection.close()
 
 def createToken(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def createSession(id):
+    connection, cursor = connectDatabase()
     token = createToken()
     now = datetime.now().strftime("%y-%m-%d %H:%M:%S")
     cursor.execute("INSERT INTO sessions ('token', 'date', 'id') VALUES (?, ?, ?)", (token, now, id))
     connection.commit()
+    connection.close()
     return id, now, token
 
 def getSessionID(token):
+    connection, cursor = connectDatabase()
     cursor.execute("SELECT 'id' FROM sessions WHERE token = ?", (token,))
     match = cursor.fetchone()
+    connection.close()
     return match[0]
 
 def sendMoney(from_id, to_id, quantity):
