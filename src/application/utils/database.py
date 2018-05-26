bf046e646c0a9f3c3ffb956e0a27b2f33149e71a
Emanuele Lillo <emalillo270304@gmail.com>
2018-05-26 18:17:36 +0200

    Make money send, transactions table and getBalance work

--- src/application/utils/database.py
+++ src/application/utils/database.py
@@ -2,73 +2,73 @@ from . import DATABASE_PATH
 import os
 import sqlite3 as sql
 from hashlib import sha512
-import string, random
+import string, random, textwrap
 from datetime import datetime
 
-def connectDatabase():
-    connection = sql.connect(DATABASE_PATH, check_same_thread=False)
-    cursor = connection.cursor()
-    return connection, cursor
+connection = sql.connect(DATABASE_PATH, check_same_thread=False)
+cursor = connection.cursor()
 
 def hashTok(tok):
     return sha512(tok.encode()).hexdigest()
 
 def getHashedPassword(id):
-    connection, cursor = connectDatabase()
     cursor.execute("SELECT * FROM credentials WHERE id = ?", (id,))
-    return cursor.fetchone()[1]
-    connection.close()
+    password = cursor.fetchone()[1]
+    return password
 
 def getBalance(id):
-    connection, cursor = connectDatabase()
-    cursor.execute("SELECT * FROM balances WHERE 'id' = ?", (id,))
-    result = cursor.fetchone()
-    print("Balance:", result) # DEBUG: SHOW ALL ROWS
-    connection.close()
-    return result[1]
+    cursor.execute("SELECT quantity FROM balances WHERE id = ?", (id,))
+    result = []
+    for r in cursor.fetchall():
+        result.append(r)
+    # print("Result:", result)
+    return result[0][0]
 
 def addBalance(id, quantity):
-    connection, cursor = connectDatabase()
     balance = getBalance(id)
     new_balance = int(balance) + quantity
     cursor.execute("UPDATE balances SET quantity = ? WHERE id = ?", (new_balance, id))
     connection.commit()
-    connection.close()
 
 def createUser(id, password):
-    connection, cursor = connectDatabase()
     hashed_password = hashTok(password)
     cursor.execute("INSERT INTO credentials ('id', 'passoword') VALUES (?, ?)", (id, hashed_password))
     cursor.execute("INSERT INTO balances ('id', 'quantity') VALUES (?, 0)", (id,))
     connection.commit()
-    connection.close()
 
 def createToken(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def createSession(id):
-    connection, cursor = connectDatabase()
     token = createToken()
     now = datetime.now().strftime("%y-%m-%d %H:%M:%S")
     cursor.execute("INSERT INTO sessions ('token', 'date', 'id') VALUES (?, ?, ?)", (token, now, id))
     connection.commit()
-    connection.close()
     return id, now, token
 
 def getSessionID(token):
-    connection, cursor = connectDatabase()
-    cursor.execute("SELECT 'id' FROM sessions WHERE token = ?", (token,))
-    match = cursor.fetchone()
-    connection.close()
-    return match[0]
+    cursor.execute("SELECT * FROM sessions WHERE token = ?", (token,))
+    result = []
+    for r in cursor.fetchall():
+        result.append(r)
+    return result[0][2]
 
 def sendMoney(from_id, to_id, quantity):
     from_balance = getBalance(from_id)
     to_balance = getBalance(to_id)
     if int(quantity) > from_balance:
         return 2
-    if int(qunatity) <= 0:
+    if int(quantity) <= 0:
         return 3
     addBalance(from_id, -quantity)
     addBalance(to_id, quantity)
+    addTransaction(from_id, to_id, quantity)
     return 1
+
+def addTransaction(from_id, to_id, q):
+    now = datetime.now().strftime("%y-%m-%d %H:%M:%S")
+    unsigned = from_id + to_id + str(q) + now
+    sign = hashTok(unsigned)
+    query = "INSERT INTO transactions ('from', 'to', 'date', 'quantity', 'sign') VALUES (?, ?, ?, ?, ?)"
+    cursor.execute(query, (from_id, to_id, now, q, sign))
+    connection.commit()
