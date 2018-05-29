79ae51c5d08ef948e2467b20f536d9ede08f7ac2
Emanuele Lillo <emalillo270304@gmail.com>
2018-05-29 22:28:14 +0200

    Started a new structure

--- src/application/utils/__init__.py
+++ src/application/utils/__init__.py
@@ -1,4 +1,4 @@
 import os
 
-DATABASE_PATH = "/home/emanuele/projects/keplero/src/database/users.db"
+DATABASE_PATH = "/home/emanuele/database/keplero/users.db"
 # DATABASE_PATH = os.environ.get("DATABASE_PATH")
