b425cf67bbc54622ab7f1284031480bbe358aad5
Emanuele Lillo <emalillo270304@gmail.com>
2018-06-03 14:07:11 +0200

    Deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd src && sudo gunicorn run:app
--- src/application/utils/__init__.py
+++ src/application/utils/__init__.py
@@ -1,4 +1,4 @@
 import os
 
-DATABASE_PATH = "/home/emanuele/database/keplero/users.db"
+DATABASE_PATH = "./users.db"
 # DATABASE_PATH = os.environ.get("DATABASE_PATH")
